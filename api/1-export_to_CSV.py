b319357a1515e0e1c2837034205db469a94bf414
EXTRADEEN <dinumira@gmail.com>
2023-04-06 19:07:16 +0200

    cript to export data in the CSV format

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,12 +1,12 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """
     Using what you did in the task #0, extend your Python script
     to export data in the CSV format
 """
 
+import csv
 import requests
 from sys import argv
-import csv
 
 
 if __name__ == "__main__":
